fa7ec3626e42cc354d2eecfe577b5b0e67338658
clgbatista <c.gbatista@aol.com>
2020-09-24 19:00:57 -0300

    typo correction

--- ttec_read.m
+++ ttec_read.m
@@ -23,9 +23,9 @@ ax42_header = [];
 ax42_header = ax42header(et_callsign,et_ssid,sc_callsign,sc_ssid);
 %%
 cmd_codes = [01 02 03];
-cmd_subcodes = ['01' '02' '03'];
+cmd_subcodes = [01 02 03];
 code = cmd_codes;
-subcode = hex2dec(cmd_subcodes);
+subcode = cmd_subcodes;
 information = [hex2dec('DE') hex2dec('AD')];
 info_length = length(information);
 data_packet_header = [code(1) subcode(1) info_length];
