adbed50d062014b1d77a88a753610f1e542eac49
carlos.batista <carlos.batista@ieee.org>
2020-11-20 21:42:26 -0300

    typos

--- dataPacket.m
+++ dataPacket.m
@@ -22,7 +22,6 @@
 function data_packet = dataPacket(code,subcode,information)
     cmd_code = cmdCoding(code);
     cmd_subcode = cmdSubcoding(subcode);
-    
     info_length = length(information);
     data_packet_header = [cmd_code cmd_subcode info_length];
     data_packet = [data_packet_header information];
